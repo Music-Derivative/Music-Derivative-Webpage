5d9b81fbf655038bdab023fe91269d83672688fc
Music-Derivative <112056032+Music-Derivative@users.noreply.github.com>
2022-09-13 02:51:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,40 +1,28 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // the site's root Notion page (required)
   rootNotionPageId: '3a57c1cea01c4b218d80a7ebb7518a5f',
 
-  // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
-  // basic site info (required)
   name: 'Inflextion Music',
   domain: 'https://rt23456p.notion.site/Inflexion-Music-Landing-3a57c1cea01c4b218d80a7ebb7518a5f',
   author: 'Inflextion Music Team',
 
-  // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
-  // social usernames (optional)
   twitter: 'Music_Inflexion',
   github: '',
   linkedin: '',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: false,
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
   isRedisEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
@@ -48,7 +36,6 @@ export default siteConfig({
   pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
-  // important pages
   navigationStyle: 'default'
   // navigationStyle: 'custom',
   // navigationLinks: [
